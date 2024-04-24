86405698561191d9c6d7a3daedc9085706598e88
yernar <yernar.baiginzheyev@gmail.com>
2024-04-24 21:40:10 +0500

    Initial Commit

---
+++ CompositionalLayout/MyCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  MyCollectionViewCell.swift
+//  CompositionalLayout
+//
+//  Created by Yernar Baiginzheyev on 24.04.2024.
+//
+
+import UIKit
+
+class MyCollectionViewCell: UICollectionViewCell {
+    
+}
