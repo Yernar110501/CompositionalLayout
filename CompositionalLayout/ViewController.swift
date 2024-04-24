c7e418a6bce9a429ca406e8264c077037a92e131
yernar <yernar.baiginzheyev@gmail.com>
2024-04-24 22:03:39 +0500

    basyc collection view configures

--- CompositionalLayout/ViewController.swift
+++ CompositionalLayout/ViewController.swift
@@ -8,12 +8,35 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private let collectionView = UICollectionView(frame: .zero,
+                                                  collectionViewLayout: UICollectionViewLayout())
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.addSubview(collectionView)
+        collectionView.register(MyCollectionViewCell.self,
+                                forCellWithReuseIdentifier: MyCollectionViewCell.identifier)
+        collectionView.frame = view.bounds
+        collectionView.backgroundColor = .systemBackground
+        collectionView.dataSource = self
     }
 
 
+}
+
+extension ViewController: UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 30
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: MyCollectionViewCell.identifier,
+                                                      for: indexPath) as! MyCollectionViewCell
+        return cell
+    }
+    
+    
 }
 
